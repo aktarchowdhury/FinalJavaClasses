e9bd0edad23ba9dcc53b929af34468383918330c
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-09 20:13:27 -0400

    IF Else

---
+++ LoopsInJava/src/com/aktar/loop/LoopClass1.java
@@ -0,0 +1,12 @@
+package com.aktar.loop;
+
+public class LoopClass1 {
+
+	public static void main(String[] args) {
+		for (int Book = 1; Book <= 33; Book += 3) {
+			System.out.println(Book);
+		}
+
+	}
+
+}
